aeb2856fc3ff128f9eeb1af9c7b085ca49f2ea0e
Thallys Nogueira <thallys.nogueira@estudante.ufjf.br>
2024-04-17 17:50:12 -0300

    Atualizações no layout do site

--- main.js
+++ main.js
@@ -16,3 +16,21 @@ function toggleParagraph(paragraphId) {
         icon.classList.add("fa-eye");
     }
 }
+
+// Função para calcular minha idade de forma automática
+function calcularIdade() {
+    var dataNascimento = "1995-02-22";
+    var hoje = new Date();
+    var dataNasc = new Date(dataNascimento);
+    var idade = hoje.getFullYear() - dataNasc.getFullYear();
+    var mes = hoje.getMonth() - dataNasc.getMonth();
+
+    if (mes < 0 || (mes === 0 && hoje.getDate() < dataNasc.getDate())) {
+        idade--;
+    }
+
+    document.getElementById("resultado_idade").textContent = "Olá, eu sou Thallys Nogueira, tenho " + idade + " anos e dou as boas-vindas à sua visita ao meu portfólio. Aqui, compartilho um pouco da minha trajetória, uma jornada repleta de aprendizado e desafios que moldaram tanto meu percurso acadêmico quanto profissional. Desde já, agradeço por explorar este espaço onde busco destacar minhas experiências e conquistas.";
+}
+
+// Chama a função assim que a página for carregada
+document.addEventListener("DOMContentLoaded", calcularIdade);
