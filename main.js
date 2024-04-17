62cca57fb16182404fb505d433cba09282a5bd59
Thallys Nogueira <thallys.nogueira@estudante.ufjf.br>
2024-04-17 10:26:35 -0300

    Melhorando o layout do site

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 // Função para mostrar e esconder os projetos
 function toggleParagraph(paragraphId) {
-    var paragraph = document.getElementById(paragraphId);
+    var paragraph = document.getElementById(paragraphId + "_paragraph");
+    var iframe = document.getElementById(paragraphId + "_iframe");
+    var icon = document.getElementById("icon_" + paragraphId);
+
     if (paragraph.style.display === "none" || paragraph.style.display === "") {
         paragraph.style.display = "block";
+        iframe.style.display = "block";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
     } else {
         paragraph.style.display = "none";
+        iframe.style.display = "none";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
     }
 }
